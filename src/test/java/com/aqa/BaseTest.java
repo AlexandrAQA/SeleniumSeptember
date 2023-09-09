273a30683d40c25d55090db0a5d671bea5d98242
aqaacc <mralextb@gmail.com>
2023-09-09 10:35:07 +0200

    BaseTest

--- src/test/java/com/aqa/BaseTest.java
+++ src/test/java/com/aqa/BaseTest.java
@@ -1,4 +1,33 @@
 package com.aqa;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
 public class BaseTest {
+    static WebDriver driver;
+    public static final String ZIP_CODE_ERROR =
+            "Oops, error on page. ZIP code should have 5 digits";
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //driver.manage().window().setSize(new Dimension(720, 480));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        System.out.println();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
